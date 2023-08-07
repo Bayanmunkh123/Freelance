4c68137acd7518775b9dcdd736df688df129bdd1
Tab-Mac <tab-mac@Tab-Macs-MacBook-Pro.local>
2023-08-07 18:31:53 +0800

    ..

--- src/scene/landing/home/product/utils/ListData.ts
+++ src/scene/landing/home/product/utils/ListData.ts
@@ -13,29 +13,52 @@ export interface ListDataType {
     isFav: boolean
     images: string[]
     parking: number
-    buildYear: number
+    releaseDate: Date
     floors: number
     floorNumber: number
     highlights: string[]
   }
+export interface ProductInputType {
+    estateType: string
+    constStatus: string
+    status: string
+    description: string
+    location: string
+    bedNo: number
+    bathNo: number
+    isFav: boolean
+    images: string[]
+    parking: number
+    releaseDate: number
+    floors: number
+    floorNumber: number
+    city: string
+    district: string
+    organizationId: string
+    price: number
+    roomNumber: number
+    sqr: number
+    priceSqr: number
+}
 export const ListData: ListDataType[] = [
     {
       estateType: 'Орон сууц',
-      estateStatus: 'Шинэ барилга',
+      constStatus: 'Шинэ барилга',
       status: 'Онцлох',
       location: 'Улаанбаатар хот, Баянзүрх дүүрэг ',
-      desc: 'Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola, body muscles and holding ones own breath. Though, most of these notes are not fundamentally necessary, they are such that you can use them for a good laugh, at a drinks party or for picking up women or men.',
+      description: 'Here, I focus on a range of items and features that we use in life without giving them a second thought such as Coca Cola, body muscles and holding ones own breath. Though, most of these notes are not fundamentally necessary, they are such that you can use them for a good laugh, at a drinks party or for picking up women or men.',
       address: ' Цагаан7 хороолол Golden Art - Эрдэнэт, Монгол',
       roomNo: 2,
       bedNo: 2,
       bathNo: 1,
-      area: 37,
+      sqr: 37,
       price: 95000000,
       isFav: false,
       images: ['https://images.homes.com/listings/214/1007620603-872007331-original.jpg', 'https://images.homes.com/listings/214/1007620603-872007331-original.jpg', 'https://images.homes.com/listings/214/1007620603-872007331-original.jpg','https://images.homes.com/listings/214/1007620603-872007331-original.jpg'],
       parking: 2,
-      buildYear: 2023,
+      releaseDate: 2023,
       floors: 12,
+      priceSqr: 1,
       floorNumber: 3,
       highlights: ['Ипотекийн зээл боломжтой', 'Ипотекийн зээл боломжтой','Ипотекийн зээл боломжтой','Ипотекийн зээл боломжтой']
     }
