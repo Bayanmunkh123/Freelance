c8c77767913a83ff5b220cf041ecaf03f0c5da1c
Bayanmunkh123 <battomorbaynmonkh@gmail.com>
2023-08-11 12:21:27 +0800

    ...

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -14,7 +14,7 @@ const navigation = (): VerticalNavItemsType => {
     {
       title: "Компаниуд",
       path: "/admin/users",
-      icon: "mdi-building",
+      icon: "mdi-shield",
       action: "read",
       subject: "Admin_Users",
       auth: true,
@@ -36,7 +36,7 @@ const navigation = (): VerticalNavItemsType => {
       auth: true,
     },
     {
-      title: "Миний компани",
+      title: "Байгууллага",
       path: "/admin/company",
       icon: "mdi-building",
       action: "read",
@@ -44,9 +44,9 @@ const navigation = (): VerticalNavItemsType => {
       auth: true,
     },
     {
-      title: "Байгууллага",
+      title: "Миний компани",
       path: "/admin/organization",
-      icon: "mdi:account",
+      icon: "mdi:building",
       action: "read",
       subject: "Admin_Roles",
       auth: true,
